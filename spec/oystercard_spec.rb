577df3a34fdc71ce1097d4f0976c3a3e1097d79d
Ben <ben@benzeng.co.uk>
2019-08-27 15:15:41 +0100

    Created new card, and associated rspec tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 require 'oystercard'
-
-
 describe Oystercard do
-
+  describe "#initialize" do
+    it "a new card will have a balance of 0" do
+      expect(subject.balance).to eq(0)
+  end
+end
 end
