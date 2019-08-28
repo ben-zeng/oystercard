d8eddebfaa592499a424ee380cfc5a7f616cdb58
Ben <ben@benzeng.co.uk>
2019-08-28 14:18:39 +0100

    added touch in and out + deduct money

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,34 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :active
   DEFAULT_BALANCE = 0
   MAX_CAPACITY = 90
   ERROR_MAX_CAPACITY = "Error! top up beyond limit"
 
   def initialize(balance: DEFAULT_BALANCE)
     @balance = balance
+    @active = false
   end
 
   def top_up(balance)
     fail ERROR_MAX_CAPACITY if @balance + balance > MAX_CAPACITY
     @balance += balance
+  end
+
+  def deduct(balance)
+    #fail ERROR_MAX_CAPACITY if @balance + balance > MAX_CAPACITY
+    @balance -= balance
+  end
+
+  def in_journey?
+    @active
+  end
+
+  def touch_in
+    @active = true
+  end
+
+  def touch_out
+    @active = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -21,5 +21,35 @@ describe Oystercard do
     it "raises error when the balance is higher then the max limit" do
       expect { subject.top_up(110) }.to raise_error Oystercard::ERROR_MAX_CAPACITY
     end
+    describe "#deduct" do
+      it "balance decreases by the amount specified" do
+        balance_before = subject.balance
+        subject.deduct(10)
+        expect((subject.balance) - balance_before).to eq(-10)
+      end
+    end
+
+    describe "#in_journey?" do
+      it "returns true if in a journey" do
+        expect(subject.in_journey?).to eq(subject.active)
+      end
+    end
+
+    describe "#touch_in" do
+      it "if not in an active journey, set @active true" do
+        subject.touch_in
+        expect(subject.in_journey?).to eq(true)
+      end
+
+    end
+
+    describe "#touch_out" do
+
+      it "if in an active journey, set @active false" do
+        subject.touch_out
+        expect(subject.in_journey?).to eq(false)
+      end
+    end
+
   end
 end
